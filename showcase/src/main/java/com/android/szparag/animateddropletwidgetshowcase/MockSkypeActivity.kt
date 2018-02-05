974841c4dc3e3228cd0730eb8ab28853b43912a6
ciemek <sharaquss@gmail.com>
2018-02-05 20:05:30 +0100

    Mock Map Activity (start)

--- showcase/src/main/java/com/android/szparag/animateddropletwidgetshowcase/MockSkypeActivity.kt
+++ showcase/src/main/java/com/android/szparag/animateddropletwidgetshowcase/MockSkypeActivity.kt
@@ -5,9 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.view.WindowManager
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -15,48 +12,36 @@ import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_mock_skype.*
 import java.util.concurrent.TimeUnit
 
-class MockSkypeActivity : AppCompatActivity() {
+class MockSkypeActivity : BaseMockActivity() {
 
   private lateinit var callTimerDisposable: Disposable
 
-  companion object {
-    fun getStartingIntent(packageContext: Context) =
-        Intent(packageContext, MockSkypeActivity::class.java)
+  companion object IntentFactory {
+    fun getStartingIntent(packageContext: Context) = Intent(packageContext, MockSkypeActivity::class.java)
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-      window.statusBarColor = resources.getColor(R.color.mockSkypeStatusBarColour)
-      window.navigationBarColor = resources.getColor(R.color.mockSkypeNavigationBarColour)
+      setStatusAndNavigationBarColour(R.color.mockSkypeStatusBarColour, R.color.mockSkypeNavigationBarColour)
     }
     setContentView(R.layout.activity_mock_skype)
-    callTimerDisposable = Observable.interval(1, TimeUnit.SECONDS)
-      .observeOn(AndroidSchedulers.mainThread())
-      .doOnSubscribe {
-        setCallDurationTimeTextView(0)
-      }.subscribeBy { seconds ->
-        setCallDurationTimeTextView(seconds)
-      }
+    callTimerDisposable = Observable
+        .interval(1, TimeUnit.SECONDS)
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnSubscribe { setCallDurationTimeTextView(0) }
+        .subscribeBy { seconds -> setCallDurationTimeTextView(seconds) }
   }
 
 
   override fun onStart() {
     super.onStart()
-    showPresetSnackbar()
+    showPresetSnackbar(R.string.showcase_screen_skype_snackbar_content)
   }
 
   override fun onDestroy() {
     super.onDestroy()
     callTimerDisposable.dispose()
-  }
-
-  private fun showPresetSnackbar() {
-    Snackbar
-      .make(findViewById(android.R.id.content), resources.getString(R.string.showcase_screen_skype_snackbar_content), Snackbar.LENGTH_LONG)
-      .show()
   }
 
   @SuppressLint("SetTextI18n")
--- showcase/src/main/java/com/android/szparag/animateddropletwidgetshowcase/ShowcaseActivity.kt
+++ showcase/src/main/java/com/android/szparag/animateddropletwidgetshowcase/ShowcaseActivity.kt
@@ -1,6 +1,5 @@
 package com.android.szparag.animateddropletwidgetshowcase
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_showcase.*
@@ -10,8 +9,8 @@ class ShowcaseActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_showcase)
-    screenPickerSkype.setOnClickListener {
-      startActivity(MockSkypeActivity.getStartingIntent(this))
-    }
+    screenPickerSkype.setOnClickListener { startActivity(MockSkypeActivity.getStartingIntent(this)) }
+    screenPickerMaps.setOnClickListener { startActivity(MockMapActivity.getStartingIntent(this)) }
   }
+
 }
