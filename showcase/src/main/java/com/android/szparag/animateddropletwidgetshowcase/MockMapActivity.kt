6df14aff462b56cc8cf373b3d11e20cb52a6a57f
ciemek <sharaquss@gmail.com>
2018-02-05 21:06:51 +0100

    Google map examples, half-transparent, coloured statusbar with content underneath

--- showcase/src/main/java/com/android/szparag/animateddropletwidgetshowcase/MockMapActivity.kt
+++ showcase/src/main/java/com/android/szparag/animateddropletwidgetshowcase/MockMapActivity.kt
@@ -2,6 +2,7 @@ package com.android.szparag.animateddropletwidgetshowcase
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.Rect
 import android.os.Build
 import android.os.Bundle
@@ -36,6 +37,9 @@ class MockMapActivity : BaseMockActivity() {
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
       window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//        window.statusBarColor = Color.TRANSPARENT
+//      };
     }
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -68,13 +72,21 @@ class MockMapActivity : BaseMockActivity() {
   }
 
   private fun getStatusbarHeight(): Int {
-    val rectangle = Rect()
-    val window = window
-    window.decorView.getWindowVisibleDisplayFrame(rectangle)
-    val statusBarHeight = rectangle.top
-    val contentViewTop = window.findViewById<View>(Window.ID_ANDROID_CONTENT).top
-    val titleBarHeight = contentViewTop - statusBarHeight
-    return statusBarHeight
+//    val rectangle = Rect()
+//    val window = window
+//    window.decorView.getWindowVisibleDisplayFrame(rectangle)
+//    val statusBarHeight = rectangle.top
+//    val contentViewTop = window.findViewById<View>(Window.ID_ANDROID_CONTENT).top
+//    val titleBarHeight = contentViewTop - statusBarHeight
+//    return statusBarHeight
+
+    var result = 0
+    val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (resourceId > 0) {
+      result = resources.getDimensionPixelSize(resourceId)
+    }
+    return result
   }
+
 
 }
